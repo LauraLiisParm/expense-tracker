1e795947405f89d589616c2889dfd671e066bcdd
LauraLiisParm <lauraliis.parm@gmail.com>
2025-09-03 20:39:09 +0300

    Adding tests

---
+++ src/main/java/eu/itcrafters/expense_tracker/infrastructure/rest/error/ErrorCode.java
@@ -0,0 +1,13 @@
+package eu.itcrafters.expense_tracker.infrastructure.rest.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum ErrorCode {
+    BUDGET_NOT_FOUND("Budget does not exist"),
+    BUDGET_EXISTS("Budget already exists");
+
+    private final String message;
+}
